afe863aefd05763756f12fcb4c9c814460b1c86e
Johannes Steger <jss@coders.de>
2013-03-03 23:39:19 +0100

    include manhole in demo app

--- samples/demo/demo.js
+++ samples/demo/demo.js
@@ -1,7 +1,7 @@
-angular.module('demo', ['i19']).
+angular.module('demo', ['i19', 'i19manhole']).
 controller('ctl', ['$scope', '$i19', function($scope, $i19) {
     $scope.$i19 = $i19;
     $scope.count = 1;
-    $scope.ts = function() { return new Date(); };
+    $scope.ts = function() { return "" + new Date(); };
     $scope.modal = function() { alert($i19("Well done")); };
 }]);
